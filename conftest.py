d4d99e67a1e7d4c1225bdde04f3a57710c8126aa
Elizabeth Lemeshko <lzbth.lemeshko@gmail.ru>
2021-04-02 00:13:50 +0300

    adding test_guest_can_go_to_login_page feature

--- conftest.py
+++ conftest.py
@@ -1,17 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='en',
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose browser language")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(executable_path=r'D:\Exe\chromedriver_win32\chromedriver.exe')
-    browser.implicitly_wait(5)
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(executable_path=r'D:\Exe\chromedriver_win32\chromedriver.exe', options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
