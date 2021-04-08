baf061ef636915000b3bdf47de9c37139a06f57d
Elizabeth Lemeshko <lzbth.lemeshko@gmail.ru>
2021-04-08 13:06:05 +0300

    first approach to navigating between pages: return the desired Page Object.

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+link = "http://selenium1py.pythonanywhere.com"
 
 
 # def test_guest_can_go_to_login_page(browser):
@@ -10,7 +10,14 @@ link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 #     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
-def test_guest_should_see_login_link(browser):
-    page = LoginPage(browser, link)
+# def test_guest_should_see_login_link(browser):
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_url()
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_url()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
