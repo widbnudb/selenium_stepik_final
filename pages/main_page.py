cf4deae9173bd70d835285ae4465b2d9e6125b4d
Elizabeth Lemeshko <lzbth.lemeshko@gmail.ru>
2021-04-08 13:10:14 +0300

    second approach to navigating between pages: the transition occurs implicitly, we initialize the page in the body of the test

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,7 +8,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        return LoginPage(browser=self.browser, url=self.browser.curren_url)
+        #return LoginPage(browser=self.browser, url=self.browser.curren_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
